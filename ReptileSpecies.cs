da67d14304fe4ebf43142980c559d448c1cde817
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-01-21 08:27:36 +0000

    create enum ReptileSpecies

---
+++ ReptileSpecies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS
+{
+    public enum ReptileSpecies
+    {
+        Turtle,
+        Lizard
+    }
+}
