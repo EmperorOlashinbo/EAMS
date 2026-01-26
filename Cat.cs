edce9f7791b5ee9fbb398222712a5eb2c434d6f4
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-01-26 19:49:23 +0000

    Complete implementation of cat class

--- Cat.cs
+++ Cat.cs
@@ -22,5 +22,34 @@ namespace EAMS
             _furColor = string.Empty;
             _isIndoor = false;
         }
+        /// <summary>
+        /// Parameterized constructor passing to base.
+        /// </summary>
+        /// <param name="numberOfTeeth">Number of teeth</param>
+        /// <param name="tailLength">Tail length in cm</param>
+        public Cat(int numberOfTeeth, double tailLength) : base(numberOfTeeth, tailLength)
+        {
+            _furColor = string.Empty;
+            _isIndoor = false;
+        }
+        public string FurColor
+        {
+            get => _furColor;
+            set => _furColor = value;
+        }
+        public bool IsIndoor
+        {
+            get => _isIndoor;
+            set => _isIndoor = value;
+        }
+        ///<summary>
+        /// Returns a string representation of the cat.
+        /// </summary>
+        /// <returns>String with cat data</returns>
+        public override string ToString()
+        {
+            return base.ToString() + $"\nFur Color: {FurColor}\nIs Indoor: {IsIndoor}";
+        }
+
     }
 }
