a7d59a2da058c6992e0ebfe2bc58c776c7f30a0c
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-01-26 19:35:41 +0000

    Implement cat class

---
+++ Cat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS
+{
+    /// <summary>
+    /// Concrete class for cats, inheriting from Mammal.
+    /// </summary>
+    public class Cat : Mammal
+    {
+        private string _furColor;
+        private bool _isIndoor;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Cat() : base()
+        {
+            _furColor = string.Empty;
+            _isIndoor = false;
+        }
+    }
+}
