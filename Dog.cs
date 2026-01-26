16006f4dea3b920296af40bb90d8ab905a204963
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-01-26 18:55:48 +0000

    Implementing dog class

---
+++ Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS
+{
+    /// <summary>
+    /// Concrete class for dogs, inheriting from Mammal.
+    /// </summary>
+    public class Dog : Mammal
+    {
+        private string _breed;
+        private bool _isTrained;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Dog() : base()
+        {
+            _breed = string.Empty;
+            _isTrained = false;
+        }
+    }
+}
