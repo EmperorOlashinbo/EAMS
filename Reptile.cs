2a604c87cf76317720bfb8078e54822332029697
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-01-26 21:10:17 +0000

    Complete implementation of reptile class

--- Reptile.cs
+++ Reptile.cs
@@ -24,5 +24,48 @@ namespace EAMS
             _livesInWater = false;
             _aggressivenessLevel = 0;
         }
+        /// <summary>
+        /// Parameterized constructor.
+        /// </summary>
+        /// <param name="bodyLength">Body length in cm</param>
+        /// <param name="livesInWater">Indicates if the reptile lives in water</param>
+        /// <param name="aggressivenessLevel">Aggressiveness level from 0 to 10</param>
+        public Reptile(double bodyLength, bool livesInWater, int aggressivenessLevel) : base()
+        {
+            _bodyLength = bodyLength >= 0 ? bodyLength : 0.0;
+            _livesInWater = livesInWater;
+            _aggressivenessLevel = (aggressivenessLevel >= 0 && aggressivenessLevel <= 10) ? aggressivenessLevel : 0;
+        }
+        public double BodyLength
+        {
+            get => _bodyLength;
+            set
+            {
+                if (value >= 0)
+                    _bodyLength = value;
+            }
+        }
+        public bool LivesInWater
+        {
+            get => _livesInWater;
+            set => _livesInWater = value;
+        }
+        public int AggressivenessLevel
+        {
+            get => _aggressivenessLevel;
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    _aggressivenessLevel = value;
+            }
+        }
+        /// <summary>
+        /// Return a string representation including base and reptile.
+        /// </summary>
+        /// <returns>String with reptile data</returns>
+        public override string ToString()
+        {
+            return base.ToString() + $"\nBody Length: {BodyLength}\nLives in Water: {LivesInWater}\nAggressiveness Level: {AggressivenessLevel}";
+        }
     }
 }
