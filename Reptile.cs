e460df8aa6d3a852c19ab468fcff692ed889771c
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-01-26 20:56:52 +0000

    Implementing reptile class

---
+++ Reptile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS
+{
+    /// <summary>
+    /// Derived class for reptiles, inheriting from Animal.
+    /// </summary>
+    public class Reptile : Animal
+    {
+        private double _bodyLength;
+        private bool _livesInWater;
+        private int _aggressivenessLevel;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Reptile() : base()
+        {
+            _bodyLength = 0.0;
+            _livesInWater = false;
+            _aggressivenessLevel = 0;
+        }
+    }
+}
