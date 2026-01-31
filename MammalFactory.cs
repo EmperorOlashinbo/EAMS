4293bbd0787ee59915f884601a839d97d12269d6
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-01-31 20:46:41 +0000

    Implement dynamic reptile fatctory class

--- MammalFactory.cs
+++ MammalFactory.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace EAMS
 {
-    internal class MammalFactory
+    /// <summary>
+    /// Factory class for creating Mammal objects.
+    /// </summary>
+    public static class MammalFactory
     {
+        /// <summary>
+        /// Creates a Mammal object based on the specified species.
+        /// </summary>
+        /// <param name="species">The mammal species.</param>
+        /// <param name="numberOfTeeth">The number of teeth.</param>
+        /// <param name="tailLength">The tail length.</param>
+        /// <returns>A Mammal instance.</returns>
+        public static Mammal CreateMammal(MammalSpecies species, int numberOfteeth, double tailLength)
+        {
+            switch (species)
+            {
+                case MammalSpecies.Dog:
+                    return new Dog(numberOfteeth, tailLength);
+                case MammalSpecies.Cat:
+                    return new Cat(numberOfteeth, tailLength);
+                default:
+                    throw new ArgumentException("Invalid mammal species");
+            }
+        }
     }
 }
---
+++ ReptileFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS
+{
+    /// <summary>
+    /// Factory class for creating reptile objects dynamically.
+    /// </summary>
+    public static class ReptileFactory
+    {
+        /// <summary>
+        /// Creates a reptile based on species and category data.
+        /// </summary>
+        /// <param name="species">The reptile species.</param>
+        /// <param name="bodyLength">Body length.</param>
+        /// <param name="livesInWater">Lives in water.</param>
+        /// <param name="aggressivenessLevel">Aggressiveness level.</param>
+        /// <returns>A Reptile instance.</returns>
+        public static Reptile CreateReptile(ReptileSpecies species, double bodyLength, bool livesInWater, int aggressivenessLevel)
+        {
+            switch (species)
+            {
+                case ReptileSpecies.Turtle:
+                    return new Turtle(bodyLength, livesInWater, aggressivenessLevel);
+                case ReptileSpecies.Lizard:
+                    return new Lizard(bodyLength, livesInWater, aggressivenessLevel);
+                default:
+                    throw new ArgumentException("Invalid species.");
+            }
+        }
+    }
+}
