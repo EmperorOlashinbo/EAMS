12cce4952596d4479ffc25494ed83ee7eb45dbf3
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-01 11:49:51 +0000

    Implement general group of reptile in reptile form

--- ReptileForm.cs
+++ ReptileForm.cs
@@ -37,8 +37,44 @@ namespace EAMS
         private CheckBox chkCanClimb;
 
         private Button btnOK;
-        private Button btnCancel;
+        private Button btnCancel; 
 
-        
+        public ReptileForm(ReptileSpecies species)
+        {
+            _species = species;
+            this.Text = $"Enter {_species} Data";
+            this.Size = new Size(400, 500);
+
+            // General group
+            GroupBox grpGeneral = new GroupBox { Text = "General Data", Location = new Point(10, 10), Size = new Size(360, 150) };
+            Controls.Add(grpGeneral);
+
+            Label lblName = new Label { Text = "Name:", Location = new Point(10, 20) };
+            txtName = new TextBox { Location = new Point(100, 20) };
+            grpGeneral.Controls.Add(lblName);
+            grpGeneral.Controls.Add(txtName);
+
+            Label lblAge = new Label { Text = "Age:", Location = new Point(10, 50) };
+            numAge = new NumericUpDown { Location = new Point(100, 50), Maximum = 100 };
+            grpGeneral.Controls.Add(lblAge);
+            grpGeneral.Controls.Add(numAge);
+
+            Label lblWeight = new Label { Text = "Weight:", Location = new Point(10, 80) };
+            numWeight = new NumericUpDown { Location = new Point(100, 80), DecimalPlaces = 2, Maximum = 1000 };
+            grpGeneral.Controls.Add(lblWeight);
+            grpGeneral.Controls.Add(numWeight);
+
+            Label lblGender = new Label { Text = "Gender:", Location = new Point(10, 110) };
+            cmbGender = new ComboBox { Location = new Point(100, 110), DropDownStyle = ComboBoxStyle.DropDownList };
+            foreach (GenderType g in Enum.GetValues(typeof(GenderType)))
+            {
+                cmbGender.Items.Add(g);
+            }
+            cmbGender.SelectedIndex = 2; 
+            grpGeneral.Controls.Add(lblGender);
+            grpGeneral.Controls.Add(cmbGender);
+
+            
+        }
     }
 }
