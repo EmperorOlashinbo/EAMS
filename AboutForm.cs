fcb257d2c42be20310a53ae7fc6a749d28163855
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-01 12:55:59 +0000

    Implement about form

---
+++ AboutForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAMS
+{
+    /// <summary>
+    /// About box form.
+    /// </summary>
+    public class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            this.Text = "About";
+            this.Size = new Size(300, 150);
+
+            Label lblInfo = new Label { Text = "EcoPark Animal Management System\nDeveloped by: Ibrahim\nVersion: 1.0", Location = new Point(10, 10), AutoSize = true };
+            Controls.Add(lblInfo);
+
+        }
+    }
+}
