033d63b6e2dc3448dfb5fb193179663043518700
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-01 06:28:12 +0000

    Initialize components of main form

--- MainForm.cs
+++ MainForm.cs
@@ -10,11 +10,59 @@ using System.Windows.Forms;
 
 namespace EAMS
 {
+    /// <summary>
+    /// Main form for the application.
+    /// </summary>
     public partial class MainForm : Form
     {
+        private Animal currentAnimal;
+        private CheckBox chkListAll;
+        private ComboBox cmbCategory;
+        private ComboBox cmbSpecies;
+        private ListBox lstAllAnimals;
+        private Button btnCreate;
+        private TextBox txtOutput;
+        private Button btnAbout;
+
         public MainForm()
         {
             InitializeComponent();
+            this.Text = "EcoPark Animal Management System";
+            this.Size = new Size(600, 400);
+
+            chkListAll = new CheckBox { Text = "List all animals", Location = new Point(10, 10) };
+            chkListAll.CheckedChanged += ChkListAll_CheckedChanged;
+            Controls.Add(chkListAll);
+
+            cmbCategory = new ComboBox { Location = new Point(10, 40), DropDownStyle = ComboBoxStyle.DropDownList };
+            cmbCategory.Items.Add("Mammal");
+            cmbCategory.Items.Add("Reptile");
+            cmbCategory.SelectedIndexChanged += CmbCategory_SelectedIndexChanged;
+            Controls.Add(cmbCategory);
+
+            cmbSpecies = new ComboBox { Location = new Point(200, 40), DropDownStyle = ComboBoxStyle.DropDownList };
+            Controls.Add(cmbSpecies);
+
+            lstAllAnimals = new ListBox { Location = new Point(10, 40), Size = new Size(300, 20), Visible = false };
+            lstAllAnimals.Items.Add("Dog");
+            lstAllAnimals.Items.Add("Cat");
+            lstAllAnimals.Items.Add("Turtle");
+            lstAllAnimals.Items.Add("Lizard");
+            lstAllAnimals.SelectedIndexChanged += LstAllAnimals_SelectedIndexChanged;
+            Controls.Add(lstAllAnimals);
+
+            btnCreate = new Button { Text = "Create Animal", Location = new Point(10, 70) };
+            btnCreate.Click += BtnCreate_Click;
+            Controls.Add(btnCreate);
+
+            txtOutput = new TextBox { Multiline = true, ReadOnly = true, Location = new Point(10, 100), Size = new Size(560, 180) };
+            Controls.Add(txtOutput);
+
+            btnAbout = new Button { Text = "About", Location = new Point(10, 300) };
+            btnAbout.Click += BtnAbout_Click;
+            Controls.Add(btnAbout);
         }
+
+        
     }
 }
