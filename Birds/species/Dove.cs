8fe4a3a72459ed225bcddafd1c38a1ba9c9ad4ab
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-03 21:12:10 +0000

    Implement species class for dove.

---
+++ Birds/species/Dove.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS.Birds.species
+{
+    /// <summary>
+    /// Concrete species class for doves, inheriting from Bird.
+    /// </summary>
+    public class Dove : Bird
+    {
+        private string _featherColor;
+
+        public Dove() : base() { _featherColor = string.Empty; }
+
+        public Dove(double wingspan, double tailLength) : base(wingspan, tailLength)
+        {
+            _featherColor = string.Empty;
+        }
+
+        public string FeatherColor
+        {
+            get => _featherColor;
+            set => _featherColor = value ?? string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nFeather Color: {FeatherColor}";
+        }
+    }
+}
