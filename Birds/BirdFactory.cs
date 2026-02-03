a0ec97f9a6181e2fe77e5c8113ce8f134911de8e
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-03 21:05:44 +0000

    Implement bird factory class

---
+++ Birds/BirdFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace EAMS.Birds
+{
+    /// <summary>
+    /// Factory class for creating bird objects dynamically.
+    /// </summary>
+    public static class BirdFactory
+    {
+        /// <summary>
+        /// Creates a bird based on species and category data.
+        /// </summary>
+        public static Bird CreateBird(BirdSpecies species, double wingspan, double tailLength)
+        {
+            switch (species)
+            {
+                case BirdSpecies.Eagle:
+                    return new Eagle(wingspan, tailLength);
+                case BirdSpecies.Dove:
+                    return new Dove(wingspan, tailLength);
+                default:
+                    throw new ArgumentException("Invalid bird species.");
+            }
+        }
+    }
+}
