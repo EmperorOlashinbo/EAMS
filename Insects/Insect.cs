11df6ef8ce452dd1b3f58d75afce0d7893c80aa7
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-04 19:49:23 +0000

    Create insect class.

--- Insects/Insect.cs
+++ Insects/Insect.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace EAMS.Insects
 {
-    internal class Insect
+    /// <summary>
+    /// Insect class inheriting from Animal.
+    /// </summary>
+    public class Insect : Animal
     {
+        private int _numberOfWings;
+        private double _antennaLength;
+
+        public Insect() : base()
+        {
+            _numberOfWings = 0;
+            _antennaLength = 0.0;
+        }
+
+        public Insect(int numberOfWings, double antennaLength) : base()
+        {
+            NumberOfWings = numberOfWings;
+            AntennaLength = antennaLength;
+        }
+
+        public int NumberOfWings
+        {
+            get => _numberOfWings;
+            set => _numberOfWings = value >= 0 ? value : 0;
+        }
+
+        public double AntennaLength
+        {
+            get => _antennaLength;
+            set => _antennaLength = value >= 0 ? value : 0.0;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $"\nNumber of Wings: {NumberOfWings}" +
+                   $"\nAntenna Length: {AntennaLength} mm";
+        }
     }
 }
