772477975fe3b304c2bdd48b84d13f045c1ce023
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-04 19:52:54 +0000

    Create insect factory class.

---
+++ Insects/InsectFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS.Insects
+{
+    public static class InsectFactory
+    {
+        public static Insect CreateInsect(InsectSpecies species, int numberOfWings, double antennaLength)
+        {
+            switch (species)
+            {
+                case InsectSpecies.Butterfly:
+                    return new Butterfly(numberOfWings, antennaLength);
+                case InsectSpecies.Bee:
+                    return new Bee(numberOfWings, antennaLength);
+                case InsectSpecies.Ant:
+                    return new Ant(numberOfWings, antennaLength);
+                default:
+                    throw new ArgumentException("Invalid insect species.");
+            }
+        }
+    }
+}
