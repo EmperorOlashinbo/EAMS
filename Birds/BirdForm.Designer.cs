be40fa8ef64021656eb29171e37fa4b6a8f68f4c
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-04 07:55:39 +0000

    creating bird form.

---
+++ Birds/BirdForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace EAMS.Birds
+{
+    partial class BirdForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BirdForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "BirdForm";
+            this.Text = "BirdForm";
+            this.Load += new System.EventHandler(this.BirdForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Birds/BirdForm.cs
@@ -0,0 +1,41 @@
+﻿using EAMS.Birds.species;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAMS.Birds
+{
+    public partial class BirdForm : Form
+    {
+        private BirdSpecies _species;
+        public Animal Animal { get; private set; }
+
+        // Common controls (same as Mammal/Reptile)
+        private TextBox txtName;
+        private NumericUpDown numAge;
+        private NumericUpDown numWeight;
+        private ComboBox cmbGender;
+        private TextBox txtImagePath;
+        private Button btnLoadImage;
+        private PictureBox picImage;
+
+        // Category controls
+        private NumericUpDown numWingspan;
+        private NumericUpDown numTailLength;
+
+        // Species-specific
+        private CheckBox chkIsBald;             
+        private TextBox txtFeatherColor;
+
+        private Button btnOK;
+        private Button btnCancel;
+
+        
+    }
+}
