e90c81c4353084e6cc13b6b1faa05d476f0e98a3
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-04 20:05:24 +0000

    Create ant class.

---
+++ Insects/Species/Ant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS.Insects.Species
+{
+    public class Ant : Insect
+    {
+        private bool _isWorker;
+
+        public Ant() : base() { _isWorker = true; }
+
+        public Ant(int numberOfWings, double antennaLength) : base(numberOfWings, antennaLength)
+        {
+            _isWorker = true;
+        }
+
+        public bool IsWorker
+        {
+            get => _isWorker;
+            set => _isWorker = value;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nIs Worker: {IsWorker}";
+        }
+    }
+}
