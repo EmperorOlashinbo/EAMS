c3a92b6dd6379074dd4d7c41860a390ff0a0360b
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-04 19:58:46 +0000

    Create butterfly class.

---
+++ Insects/Species/Butterfly.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS.Insects.Species
+{
+    public class Butterfly : Insect
+    {
+        private string _wingPattern;
+
+        public Butterfly() : base() { _wingPattern = string.Empty; }
+
+        public Butterfly(int numberOfWings, double antennaLength) : base(numberOfWings, antennaLength)
+        {
+            _wingPattern = string.Empty;
+        }
+
+        public string WingPattern
+        {
+            get => _wingPattern;
+            set => _wingPattern = value ?? string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nWing Pattern: {WingPattern}";
+        }
+    }
+}
