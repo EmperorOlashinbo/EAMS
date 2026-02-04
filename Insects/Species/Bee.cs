7615e39a961e72df360a15e2e58e36de75188b23
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-04 20:03:43 +0000

    Create bee class.

---
+++ Insects/Species/Bee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS.Insects.Species
+{
+    public class Bee : Insect
+    {
+        private bool _canSting;
+        private double _honeyProductionPerDay;  // in grams
+
+        public Bee() : base()
+        {
+            _canSting = true;
+            _honeyProductionPerDay = 0.0;
+        }
+
+        public Bee(int numberOfWings, double antennaLength) : base(numberOfWings, antennaLength)
+        {
+            _canSting = true;
+            _honeyProductionPerDay = 0.0;
+        }
+
+        public bool CanSting
+        {
+            get => _canSting;
+            set => _canSting = value;
+        }
+
+        public double HoneyProductionPerDay
+        {
+            get => _honeyProductionPerDay;
+            set => _honeyProductionPerDay = value >= 0 ? value : 0.0;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $"\nCan Sting: {CanSting}" +
+                   $"\nHoney Production (g/day): {HoneyProductionPerDay}";
+        }
+    }
+}
