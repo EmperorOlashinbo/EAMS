ee7209d3312be8b8cfdb9c86570632da1c8ce67a
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-07 20:26:52 +0000

    Improve layout and spacing of the main form.

--- MainForm.cs
+++ MainForm.cs
@@ -1,222 +1,318 @@
 ﻿using EAMS.Birds;
 using EAMS.Insects;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace EAMS
 {
     /// <summary>
-    /// Main form for the application.
+    /// Main form for the EcoPark Animal Management System.
+    /// Handles category/species selection and displays created animals.
     /// </summary>
     public partial class MainForm : Form
     {
         private Animal currentAnimal;
+
         private CheckBox chkListAll;
-        private ComboBox cmbCategory;
-        private ComboBox cmbSpecies;
+        private ListBox lstCategory;
+        private ListBox lstSpecies;
         private ListBox lstAllAnimals;
         private Button btnCreate;
         private TextBox txtOutput;
         private Button btnAbout;
+        private Button btnClear;
+        private PictureBox picPreview; 
+
+        // General data controls
+        private GroupBox grpGeneral;
+        private Label lblName;
+        private TextBox txtName;
+        private Label lblAge;
+        private TextBox txtAge;
+        private Label lblWeight;
+        private TextBox txtWeight;
+        private Label lblGender;
+        private ComboBox cmbGender;
+        private Button btnAdd;
+
+        private Button btnLoadImage;
 
         public MainForm()
         {
             InitializeComponent();
             this.Text = "EcoPark Animal Management System";
-            this.Size = new Size(600, 400);
+            this.Size = new Size(760, 540);
+            this.StartPosition = FormStartPosition.CenterScreen;
 
-            chkListAll = new CheckBox { Text = "List all animals", Location = new Point(10, 10) };
+            // Menu bar
+            MenuStrip menu = new MenuStrip();
+            ToolStripMenuItem mnuFile = new ToolStripMenuItem("File");
+            ToolStripMenuItem mnuHelp = new ToolStripMenuItem("Help");
+            ToolStripMenuItem mnuAbout = new ToolStripMenuItem("About");
+            mnuAbout.Click += BtnAbout_Click;
+            mnuHelp.DropDownItems.Add(mnuAbout);
+            menu.Items.Add(mnuFile);
+            menu.Items.Add(mnuHelp);
+            this.MainMenuStrip = menu;
+            Controls.Add(menu);
+
+            // Create Animal label + lists
+            Label lblCreate = new Label { Text = "Create Animal", Location = new Point(10, 30), AutoSize = true };
+            Controls.Add(lblCreate);
+
+            lstCategory = new ListBox { Location = new Point(10, 55), Size = new Size(120, 120) };
+            lstCategory.Items.AddRange(new[] { "Bird", "Insect", "Mammal", "Reptile" });
+            lstCategory.SelectedIndexChanged += LstCategory_SelectedIndexChanged;
+            Controls.Add(lstCategory);
+
+            lstSpecies = new ListBox { Location = new Point(140, 55), Size = new Size(120, 120) };
+            Controls.Add(lstSpecies);
+
+            chkListAll = new CheckBox { Text = "List all animal species", Location = new Point(10, 180), AutoSize = true };
             chkListAll.CheckedChanged += ChkListAll_CheckedChanged;
             Controls.Add(chkListAll);
 
-            cmbCategory = new ComboBox { Location = new Point(10, 40), DropDownStyle = ComboBoxStyle.DropDownList };
-            cmbCategory.Items.Add("Mammal");
-            cmbCategory.Items.Add("Reptile");
-            cmbCategory.Items.Add("Bird");
-            cmbCategory.Items.Add("Insect");
-            cmbCategory.SelectedIndexChanged += CmbCategory_SelectedIndexChanged;
-            Controls.Add(cmbCategory);
-
-            cmbSpecies = new ComboBox { Location = new Point(200, 40), DropDownStyle = ComboBoxStyle.DropDownList };
-            Controls.Add(cmbSpecies);
-
-            lstAllAnimals = new ListBox { Location = new Point(10, 40), Size = new Size(300, 20), Visible = false };
-            lstAllAnimals.Items.Add("Dog");
-            lstAllAnimals.Items.Add("Cat");
-            lstAllAnimals.Items.Add("Cow");
-            lstAllAnimals.Items.Add("Horse");
-            lstAllAnimals.Items.Add("Turtle");
-            lstAllAnimals.Items.Add("Lizard");
-            lstAllAnimals.Items.Add("Snake");
-            lstAllAnimals.Items.Add("Eagle");
-            lstAllAnimals.Items.Add("Dove");
-            lstAllAnimals.Items.Add("Butterfly");
-            lstAllAnimals.Items.Add("Bee");
-            lstAllAnimals.Items.Add("Ant");
+            lstAllAnimals = new ListBox { Location = new Point(10, 205), Size = new Size(250, 120), Visible = false };
+            lstAllAnimals.Items.AddRange(new[] { "Dog", "Cat", "Cow", "Horse", "Turtle", "Lizard", "Snake", "Eagle", "Dove", "Butterfly", "Bee", "Ant" });
             lstAllAnimals.SelectedIndexChanged += LstAllAnimals_SelectedIndexChanged;
             Controls.Add(lstAllAnimals);
 
-            btnCreate = new Button { Text = "Create Animal", Location = new Point(10, 70) };
+            btnCreate = new Button { Text = "Create Animal", Location = new Point(140, 180), Width = 120 };
             btnCreate.Click += BtnCreate_Click;
             Controls.Add(btnCreate);
 
-            txtOutput = new TextBox { Multiline = true, ReadOnly = true, Location = new Point(10, 100), Size = new Size(560, 180) };
-            Controls.Add(txtOutput);
+            // image preview and load image button
+            picPreview = new PictureBox
+            {
+                Location = new Point(410, 55),
+                Size = new Size(200, 150),
+                SizeMode = PictureBoxSizeMode.Zoom,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+            Controls.Add(picPreview);
 
-            btnAbout = new Button { Text = "About", Location = new Point(10, 300) };
+            btnLoadImage = new Button { Text = "Load Image", Location = new Point(410, 210), Width = 200 };
+            btnLoadImage.Click += BtnLoadImage_Click;
+            Controls.Add(btnLoadImage);
+
+            // General data group
+            grpGeneral = new GroupBox { Text = "General Data", Location = new Point(10, 340), Size = new Size(360, 150) };
+            lblName = new Label { Text = "Name", Location = new Point(10, 20) };
+            lblName.Location = new Point(10, 20);
+            lblName.AutoSize = true;
+            grpGeneral.Controls.Add(lblName);
+
+            txtName = new TextBox { Location = new Point(90, 18), Width = 240 };
+            grpGeneral.Controls.Add(txtName);
+
+            lblAge = new Label { Text = "Age", Location = new Point(10, 50), AutoSize = true };
+            grpGeneral.Controls.Add(lblAge);
+            txtAge = new TextBox { Location = new Point(90, 48), Width = 100 };
+            grpGeneral.Controls.Add(txtAge);
+
+            lblWeight = new Label { Text = "Weight", Location = new Point(10, 80), AutoSize = true };
+            grpGeneral.Controls.Add(lblWeight);
+            txtWeight = new TextBox { Location = new Point(90, 78), Width = 100 };
+            grpGeneral.Controls.Add(txtWeight);
+
+            lblGender = new Label { Text = "Gender", Location = new Point(10, 110), AutoSize = true };
+            grpGeneral.Controls.Add(lblGender);
+            cmbGender = new ComboBox { Location = new Point(90, 108), Width = 120, DropDownStyle = ComboBoxStyle.DropDownList };
+            cmbGender.Items.AddRange(new[] { "male", "female", "Unknown" });
+            grpGeneral.Controls.Add(cmbGender);
+
+            btnAdd = new Button { Text = "Add", Location = new Point(240, 108), Width = 90, Enabled = false };
+            btnAdd.Click += (s, e) => MessageBox.Show("Use the Create Animal workflow (species forms) to add animals.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            grpGeneral.Controls.Add(btnAdd);
+
+            Controls.Add(grpGeneral);
+
+            // Output box showing results of created animal
+            GroupBox grpOutput = new GroupBox { Text = "Animal Details", Location = new Point(410, 260), Size = new Size(320, 230) };
+            txtOutput = new TextBox
+            {
+                Multiline = true,
+                ReadOnly = true,
+                Dock = DockStyle.Fill,
+                ScrollBars = ScrollBars.Vertical,
+                Font = new Font("Consolas", 10)
+            };
+            grpOutput.Controls.Add(txtOutput);
+            Controls.Add(grpOutput);
+
+            // Bottom buttons
+            btnClear = new Button { Text = "Clear Output", Location = new Point(540, 500 - 40), Width = 190 };
+            btnClear.Click += (s, e) => { txtOutput.Clear(); currentAnimal = null; if (picPreview.Image != null) { picPreview.Image.Dispose(); picPreview.Image = null; } };
+            Controls.Add(btnClear);
+
+            btnAbout = new Button { Text = "About", Location = new Point(10, 500 - 40), Width = 100 };
             btnAbout.Click += BtnAbout_Click;
             Controls.Add(btnAbout);
-
         }
-
+        // Event handlers for UI interactions
+        private void BtnLoadImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog { Filter = "Images|*.png;*.jpg;*.jpeg;*.bmp;*.gif" })
+            {
+                if (dlg.ShowDialog() == DialogResult.OK && File.Exists(dlg.FileName))
+                {
+                    try
+                    {
+                        picPreview.Image?.Dispose();
+                        picPreview.Image = Image.FromFile(dlg.FileName);
+                        if (currentAnimal != null)
+                            currentAnimal.ImagePath = dlg.FileName;
+                    }
+                    catch { /* ignore image load errors */ }
+                }
+            }
+        }
+        // Event handler for "List all animal species" checkbox
         private void ChkListAll_CheckedChanged(object sender, EventArgs e)
         {
             bool isChecked = chkListAll.Checked;
-            cmbCategory.Enabled = !isChecked;
-            cmbSpecies.Enabled = !isChecked;
+            lstCategory.Enabled = !isChecked;
+            lstSpecies.Enabled = !isChecked;
             lstAllAnimals.Visible = isChecked;
             if (isChecked)
             {
-                cmbCategory.SelectedIndex = -1;
-                cmbSpecies.Items.Clear();
-                cmbSpecies.Text = string.Empty;
+                lstCategory.ClearSelected();
+                lstSpecies.Items.Clear();
             }
             else
             {
-                lstAllAnimals.SelectedIndex = -1;
+                lstAllAnimals.ClearSelected();
             }
         }
-
-        private void CmbCategory_SelectedIndexChanged(object sender, EventArgs e)
+        // Event handler for category selection change
+        private void LstCategory_SelectedIndexChanged(object sender, EventArgs e)
         {
-            cmbSpecies.Items.Clear();
-            string cat = cmbCategory.SelectedItem?.ToString();
-            if (cat == "Mammal")
+            lstSpecies.Items.Clear();
+            string cat = lstCategory.SelectedItem?.ToString() ?? "";
+            switch (cat)
             {
-                foreach (MammalSpecies sp in Enum.GetValues(typeof(MammalSpecies)))
-                {
-                    cmbSpecies.Items.Add(sp);
-                }
-            }
-            else if (cat == "Reptile")
-            {
-                foreach (ReptileSpecies sp in Enum.GetValues(typeof(ReptileSpecies)))
-                {
-                    cmbSpecies.Items.Add(sp);
-                }
-            }
-            else if (cat == "Bird")
-            {
-                foreach (BirdSpecies sp in Enum.GetValues(typeof(BirdSpecies)))
-                {
-                    cmbSpecies.Items.Add(sp);
-                }
-            }
-            else if (cat == "Insect")
-            {
-                foreach (InsectSpecies sp in Enum.GetValues(typeof(InsectSpecies)))
-                {
-                    cmbSpecies.Items.Add(sp);
-                }
+                case "Mammal":
+                    lstSpecies.Items.AddRange(Enum.GetNames(typeof(MammalSpecies)));
+                    break;
+                case "Reptile":
+                    lstSpecies.Items.AddRange(Enum.GetNames(typeof(ReptileSpecies)));
+                    break;
+                case "Bird":
+                    lstSpecies.Items.AddRange(Enum.GetNames(typeof(BirdSpecies)));
+                    break;
+                case "Insect":
+                    lstSpecies.Items.AddRange(Enum.GetNames(typeof(InsectSpecies)));
+                    break;
             }
         }
-
+        // Event handler for selecting a species from the "List all animal species" list
         private void LstAllAnimals_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (lstAllAnimals.SelectedItem != null)
+            if (lstAllAnimals.SelectedItem is string sp && !string.IsNullOrEmpty(sp))
             {
-                string sp = lstAllAnimals.SelectedItem.ToString();
                 string cat = GetCategoryFromSpecies(sp);
-                cmbCategory.Text = cat;
-                cmbSpecies.Text = sp;
+                if (!string.IsNullOrEmpty(cat))
+                {
+                    lstCategory.SelectedItem = cat;
+                    lstSpecies.SelectedItem = sp;
+                }
             }
         }
-
+        // Helper method to determine category based on species name
         private string GetCategoryFromSpecies(string species)
         {
-            if (species == "Dog" || species == "Cat" || species == "Cow" || species == "Horse") return "Mammal";
-            if (species == "Turtle" || species == "Lizard" || species == "Snake") return "Reptile";
-            if (species == "Eagle" || species == "Dove") return "Bird";
-            if (species == "Butterfly" || species == "Bee" || species == "Ant") return "Insect";
-            return string.Empty;
+            if (species == "Dog" || species == "Cat" || species == "Cow" || species == "Horse")
+                return "Mammal";
+            if (species == "Turtle" || species == "Lizard" || species == "Snake")
+                return "Reptile";
+            if (species == "Eagle" || species == "Dove")
+                return "Bird";
+            if (species == "Butterfly" || species == "Bee" || species == "Ant")
+                return "Insect";
+            return "";
         }
-
+        // Event handler for the "Create Animal" button click
         private void BtnCreate_Click(object sender, EventArgs e)
         {
-            string category, speciesStr;
-            if (!chkListAll.Checked)
-            {
-                category = cmbCategory.Text;
-                speciesStr = cmbSpecies.Text;
-            }
-            else
-            {
-                speciesStr = lstAllAnimals.SelectedItem?.ToString();
-                if (speciesStr == null) return;
-                category = GetCategoryFromSpecies(speciesStr);
-            }
+            // Determine the selected category and species based on the current UI state
+            string category = chkListAll.Checked ? GetCategoryFromSpecies(lstAllAnimals.SelectedItem?.ToString() ?? "") : lstCategory.SelectedItem?.ToString() ?? "";
+            string speciesStr = chkListAll.Checked ? lstAllAnimals.SelectedItem?.ToString() : lstSpecies.SelectedItem?.ToString();
 
             if (string.IsNullOrEmpty(category) || string.IsNullOrEmpty(speciesStr))
             {
-                MessageBox.Show("Please select a category and species.");
+                MessageBox.Show("Please select a category and species.", "Input required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-
+            // Create the appropriate form based on the selected category and species
             Form form = null;
-            if (category == "Mammal")
+            try
             {
-                if (Enum.TryParse<MammalSpecies>(speciesStr, out MammalSpecies sp))
-                {
-                    form = new MammalForm(sp);
-                }
+                if (category == "Mammal" && Enum.TryParse<MammalSpecies>(speciesStr, out var mSp))
+                    form = new MammalForm(mSp);
+                else if (category == "Reptile" && Enum.TryParse<ReptileSpecies>(speciesStr, out var rSp))
+                    form = new ReptileForm(rSp);
+                else if (category == "Bird" && Enum.TryParse<BirdSpecies>(speciesStr, out var bSp))
+                    form = new BirdForm(bSp);
+                else if (category == "Insect" && Enum.TryParse<InsectSpecies>(speciesStr, out var iSp))
+                    form = new InsectForm(iSp);
             }
-            else if (category == "Reptile")
+            catch (Exception ex)
             {
-                if (Enum.TryParse<ReptileSpecies>(speciesStr, out ReptileSpecies sp))
-                {
-                    form = new ReptileForm(sp);
-                }
+                MessageBox.Show($"Error creating form: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
-            else if (category == "Bird")
-            {
-                if (Enum.TryParse<BirdSpecies>(speciesStr, out BirdSpecies sp))
-                {
-                    form = new BirdForm(sp);
-                }
-            }
-            else if (category == "Insect")
-            {
-                if (Enum.TryParse<InsectSpecies>(speciesStr, out InsectSpecies sp))
-                {
-                    form = new InsectForm(sp);
-                }
-            }
-
+            // Show the species-specific form and create the animal
             if (form != null && form.ShowDialog() == DialogResult.OK)
             {
                 if (category == "Mammal")
                 {
                     currentAnimal = ((MammalForm)form).Animal;
                 }
-                else
+                else if (category == "Reptile")
                 {
                     currentAnimal = ((ReptileForm)form).Animal;
                 }
-                txtOutput.Text = currentAnimal.ToString();
+                else if (category == "Bird")
+                {
+                    currentAnimal = ((BirdForm)form).Animal;
+                }
+                else if (category == "Insect")
+                {
+                    currentAnimal = ((InsectForm)form).Animal;
+                }
+                else
+                {
+                    currentAnimal = null;
+                }
+                // Display the created animal's details and image
+                if (currentAnimal != null)
+                {
+                    txtOutput.Text = currentAnimal.ToString();
+                    if (!string.IsNullOrEmpty(currentAnimal.ImagePath))
+                    {
+                        try
+                        {
+                            picPreview.Image?.Dispose();
+                            picPreview.Image = Image.FromFile(currentAnimal.ImagePath);
+                        }
+                        catch { picPreview.Image = null; }
+                    }
+                    else
+                    {
+                        picPreview.Image = null;
+                    }
+                }
             }
         }
-
+        // Event handler for the "About" menu item click
         private void BtnAbout_Click(object sender, EventArgs e)
         {
-            AboutForm about = new AboutForm();
-            about.ShowDialog();
+            using (var about = new AboutForm())
+            {
+                about.ShowDialog();
+            }
         }
     }
 }
