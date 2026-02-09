7ecfff96afabfe5bfd80743b6f5f8e83958f0391
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-09 07:35:40 +0000

    making documentation.

--- Reptiles/ReptileSpecies.cs
+++ Reptiles/ReptileSpecies.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace EAMS
 {
+    /// <summary>
+    /// Enumeration for reptile species. This enum defines the different species of reptiles that can be created in the system.
+    /// </summary>
     public enum ReptileSpecies
     {
         Turtle,
