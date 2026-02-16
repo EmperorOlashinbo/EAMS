72f28497c71b0a9c711101f7d7666a437ccb0871
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-16 20:12:14 +0000

    Create animal interface.

---
+++ AnimalsGen/IAnimal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAMS.AnimalsGen
+{
+    /// <summary>
+    /// Defines the interface for an animal, which includes properties
+    /// </summary>
+    public interface IAnimal
+    {
+        string Id { get; }
+        string Name { get; set; }
+        GenderType Gender { get; set; }
+        int Age { get; set; }
+        double Weight { get; set; }
+        string ImagePath { get; set; }
+
+        string ToString();
+    }
+}
