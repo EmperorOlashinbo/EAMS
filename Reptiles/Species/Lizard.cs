d799924c119b3e2189cc5c65d0b9f515180d6f11
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-24 15:52:54 +0100

    update lizard class for V2.

--- Reptiles/Species/Lizard.cs
+++ Reptiles/Species/Lizard.cs
@@ -6,44 +6,56 @@ using System.Threading.Tasks;
 
 namespace EAMS
 {
+    /// <summary>
+    /// Concrete species class a Lizard, inheriting from Reptile.
+    /// </summary>
     public class Lizard : Reptile
     {
         private string _color;
         private bool _canClimb;
-
-        /// <summary>
-        /// Default Constructor
-        /// </summary>
-        public Lizard() : base()
-        {
-            _color = string.Empty;
-            _canClimb = false;
-        }
-        /// <summary>
-        /// Parameterized Constructor
-        /// </summary>
-        /// <param name="bodyLength">Body length.</param>
-        /// <param name="livesInWater">Lives in water</param>
-        /// <param name="aggressivenessLevel">Aggressiveness level.</param>
-        public Lizard(double bodyLength, bool livesInWater, int aggressivenessLevel) : base(bodyLength, livesInWater, aggressivenessLevel)
-        {
-            _color = string.Empty;
-            _canClimb = false;
-        }
-        /// <summary>
-        /// Gets or sets the color of the lizard. The color is represented as a string, 
-        /// which can describe the color of the lizard's skin.
-        /// </summary>
-        public string Color { get => _color; set => _color = value; }
-        /// <summary>
-        /// Gets or sets whether the lizard can climb. This property indicates if the lizard has the ability to climb surfaces,
-        /// </summary>
-        public bool CanClimb { get => _canClimb; set => _canClimb = value; }
         
         /// <summary>
-        /// returns a string representation of the Lizard object.
+        /// Default constructor. Initializes a lizard with default values.
         /// </summary>
-        /// <returns>String with lizard data</returns>
+        public Lizard() : base() { }
+        /// <summary>
+        /// Parameterized constructor. Initializes a lizard with specified values.
+        /// </summary>
+        /// <param name="bodyLength">Body length of the lizard.</param>
+        /// <param name="livesInWater">Indicates if the lizard lives in water.</param>
+        /// <param name="aggressivenessLevel">Aggressiveness level of the lizard.</param>
+        public Lizard(double bodyLength, bool livesInWater, int aggressivenessLevel)
+            : base(bodyLength, livesInWater, aggressivenessLevel) { }
+        /// <summary>
+        /// Gets or sets the color of the lizard. If a null value is assigned, it defaults to "Unknown".
+        /// </summary>
+        public string Color { get => _color; set => _color = value ?? "Unknown"; }
+        /// <summary>
+        /// Gets or sets a value indicating whether the lizard can climb.
+        /// </summary>
+        public bool CanClimb { get => _canClimb; set => _canClimb = value; }
+        /// <summary>
+        /// Gets the average lifespan of the lizard in years. 
+        /// Average lifespan for many lizard species is around 5-15 years, 
+        /// so we return 10 as a general average.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetAverageLifeSpan() => 10;
+        /// <summary>
+        /// Gets the daily food requirements of the lizard.
+        /// </summary>
+        /// <returns>A dictionary containing the daily food requirements of the lizard.</returns>
+        public override Dictionary<string, string> DailyFoodRequirement() =>
+            new Dictionary<string, string>
+            {
+                ["Morning"] = "Insects + vegetables",
+                ["Evening"] = "Fruits + protein supplement"
+            };
+        
+        /// <summary>
+        /// Returns a string representation of the lizard, including color and climbing ability.
+        /// </summary>
+        /// <returns>A string representation of the lizard.</returns>
         public override string ToString()
         {
             return base.ToString() + $"\nColor: {Color}\nCan Climb: {CanClimb}";
