39654e8581edf10bd54305f87834b38ab5d8526b
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-02-24 20:12:16 +0100

    update bee class for V2.

--- Insects/Species/Bee.cs
+++ Insects/Species/Bee.cs
@@ -12,45 +12,65 @@ namespace EAMS.Insects.Species
     public class Bee : Insect
     {
         private bool _canSting;
-        private double _honeyProductionPerDay;  // in grams
+        private double _honeyProductionPerDay;
         /// <summary>
-        /// Default constructor. Initializes the bee with stinging ability and zero honey production.
+        /// Default constructor. Initializes a bee with default values.
         /// </summary>
         public Bee() : base()
         {
             _canSting = true;
             _honeyProductionPerDay = 0.0;
         }
+        
         /// <summary>
-        /// Parameterized constructor for bee specific data, including stinging ability and honey production.
+        /// Parameterized constructor for bee specific data.
         /// </summary>
-        /// <param name="numberOfWings"></param>
-        /// <param name="antennaLength"></param>
+        /// <param name="numberOfWings">The number of wings of the bee.</param>
+        /// <param name="antennaLength">The length of the antenna of the bee in millimeters.</param>
         public Bee(int numberOfWings, double antennaLength) : base(numberOfWings, antennaLength)
         {
             _canSting = true;
             _honeyProductionPerDay = 0.0;
         }
+        
         /// <summary>
-        /// Gets or sets the stinging ability of the bee. By default, bees are considered to have stinging ability, but this can be changed if needed.
+        /// Gets or sets a value indicating whether the bee can sting.
         /// </summary>
         public bool CanSting
         {
             get => _canSting;
             set => _canSting = value;
         }
+        
         /// <summary>
-        /// Gets or sets the honey production per day in grams. Value is constrained to be non-negative.
+        /// Gets or sets the honey production per day in grams.
         /// </summary>
         public double HoneyProductionPerDay
         {
             get => _honeyProductionPerDay;
             set => _honeyProductionPerDay = value >= 0 ? value : 0.0;
         }
+        
         /// <summary>
-        /// Returns a string representation including base insect data, stinging ability, and honey production.
+        /// Gets the average lifespan of the bee in weeks.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The average lifespan of the bee in weeks.</returns>
+        public override int GetAverageLifeSpan() => 1;   // Worker bees live ~6 weeks
+        
+        /// <summary>
+        /// Gets the daily food requirements of the bee.
+        /// </summary>
+        /// <returns>A dictionary containing the daily food requirements of the bee.</returns>
+        public override Dictionary<string, string> DailyFoodRequirement() =>
+            new Dictionary<string, string>
+            {
+                ["All day"] = "Nectar and pollen"
+            };
+        
+        /// <summary>
+        /// Returns a string representation of the bee, including stinging ability and honey production.
+        /// </summary>
+        /// <returns>A string representation of the bee.</returns>
         public override string ToString()
         {
             return base.ToString() +
